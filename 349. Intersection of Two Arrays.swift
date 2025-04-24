e2c393c70bfd6e9085df4c1e57f15d9cd378082f
Mansi Nerkar <mansinerkar11@gmail.com>
2025-04-24 09:02:50 +0530

    Create 349. Intersection of Two Arrays.swift
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [9,4]
    Explanation: [4,9] is also accepted.
     
    
    Constraints:
    
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 1000

---
+++ 349. Intersection of Two Arrays.swift
@@ -0,0 +1,7 @@
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        let set1 = Set(nums1)
+        let set2 = Set(nums2)
+        return Array(set1.intersection(set2))
+    }
+}
